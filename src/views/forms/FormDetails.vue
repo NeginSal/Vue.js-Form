d7e3e24d90c78522938df5723179c7ebcaf52448
NeginSal <neginsaljooghi@gmail.com>
2022-02-14 13:47:54 +0330

    edit form detail

--- src/views/forms/FormDetails.vue
+++ src/views/forms/FormDetails.vue
@@ -1,23 +1,57 @@
 <template>
   <div>
-    <h1>this is form detail</h1>
-    <p>this is form id {{id}}</p>
-
+    <div class="details">
+      <div v-if="form">
+        <p>{{ form.firstName }}</p>
+        <p>{{ form.lastName }}</p>
+        <p>{{ form.Mobile }}</p>
+        <p>{{ form.phone }}</p>
+        <p>{{ form.address }}</p>
+        <p>{{ form.gender }}</p>
+      </div>
+      <div v-else>
+        <p>Loading form details ...</p>
+      </div>
+    </div>
+          <router-link to="/forms">بازگشت به خانه </router-link>
   </div>
 </template>
 
 <script>
 export default {
   data() {
-    return{
-      id:this.$route.params.id
-    }
-
+    return {
+      id: this.$route.params.id,
+      form: null,
+    };
   },
-
+  mounted() {
+    fetch("http://localhost:8000/forms/" + this.id)
+      .then((res) => res.json())
+      .then((data) => (this.form = data))
+      .catch((err) => console.log(err.message));
+  },
 };
 </script>
 
 <style scoped>
-
+.details {
+  padding: 20px;
+  margin: 20px auto;
+  max-width: 600px;
+  border-bottom: 1px solid #fafafa;
+  text-align: right;
+  background-color: #f2f2f2;
+  cursor: pointer;
+}
+.details p {
+  padding: 6px 10px;
+  margin: 10px 0;
+  display: block;
+  text-align: center;
+  font-size: medium;
+  color: #006666;
+  margin-bottom: 8px;
+  border-top: 2px solid grey;
+}
 </style>
