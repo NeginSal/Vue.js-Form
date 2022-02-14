6a9d8a21e6cbfd597fabafe1ad89dad80277016e
NeginSal <neginsaljooghi@gmail.com>
2022-02-14 09:34:43 +0330

    add  validation

--- src/views/CreateForms.vue
+++ src/views/CreateForms.vue
@@ -2,23 +2,47 @@
   <div class="create">
     <h6>ثبت نام</h6>
     <p>.لطفا اطلاعات خود را وارد کنید</p>
-    <form>
-      <input type="text" required placeholder="نام" />
-      <input type="text" required placeholder="نام خانوادگی" />
-      <input type="tel" required placeholder="تلفن همراه" />
-      <input type="tel" required placeholder="تلفن ثابت" />
-      <input type="text" required placeholder="آدرس دقیق" />
-      <select value="{gender}">
+    <form @submit.prevent="handleSubmit">
+      <input type="text" required placeholder="نام" v-model="firstName"/>
+      <input type="text" required placeholder="نام خانوادگی" v-model="lastName"/>
+      <input type="tel" required placeholder="تلفن همراه" v-model="Mobile"/>
+      <div v-if="MobileError" class="error">{{MobileError}}</div>
+      <input type="tel" required placeholder="تلفن ثابت" v-model="phone"/>
+      <div v-if="PhoneError" class="error">{{PhoneError}}</div>
+      <input type="text" required placeholder="آدرس دقیق" v-model="address"/>
+      <select v-model="gender">
         <option value="female">خانم</option>
         <option value="male">آقا</option>
       </select>
-      <button>مرحله بعد</button>
+      <button >مرحله بعد</button>
     </form>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data(){
+    return{
+      firstName:'',
+      lastName:'',
+      Mobile:'',
+      phone:'',
+      address:'',
+      gender:'',
+      MobileError:'',
+      PhoneError:''
+    }
+  },
+  methods:{
+  handleSubmit(){
+    //validate Mobile 
+    this.MobileError = this.Mobile.length >11 ? '' : '.شماره وارد شده باید حداقل ۱۱ رقم داشته باشد';
+    this.PhoneError = this.phone.length >11 ? '' : '.شماره وارد شده باید حداقل ۱۱ رقم داشته باشد';
+
+  }
+  }
+
+};
 </script>
 
 <style>
@@ -68,5 +92,11 @@ p {
   cursor: pointer;
   text-align: center;
   display: block;
+}
+.error{
+  color: red;
+  margin-top: 10px;
+  font-size: 0.8em;
+  font-weight: bold;
 }
 </style>
